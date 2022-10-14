866d619e918eb1286666a2e7cb22be771ce2f01e
djihane Ghilani <djihaneghilani@gmail.com>
2022-10-14 10:54:53 +0100

    added logger

--- android/src/main/kotlin/io/github/parassharmaa/usage_stats/UsageStatsPlugin.kt
+++ android/src/main/kotlin/io/github/parassharmaa/usage_stats/UsageStatsPlugin.kt
@@ -79,6 +79,8 @@ public class UsageStatsPlugin : FlutterPlugin, MethodCallHandler {
             "queryUsageStats" -> {
                 var start: Long = call.argument<Long>("start") as Long
                 var end: Long = call.argument<Long>("end") as Long
+                Log.d("START TIME :", System.currentTimeMillis() - 1000 * 3600 * 1 );
+                Log.d("END TIME :",   System.currentTimeMillis());
                 result.success(UsageStats.queryUsageStats(mContext!!, start, end))
             }
             "queryNetworkUsageStats" -> {
