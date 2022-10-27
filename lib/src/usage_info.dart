92a1ea10a0a742416ada90b9e33ca9f591b22b01
djihane Ghilani <djihaneghilani@gmail.com>
2022-10-27 17:52:52 +0100

    removed last time

--- lib/src/usage_info.dart
+++ lib/src/usage_info.dart
@@ -5,22 +5,20 @@ class UsageInfo {
     this.lastTimeUsed,
     this.packageName,
     this.totalTimeInForeground,
-    this.lastTimeForegroundServiceUsed,
   });
 
   /// Construct class from the json map
   factory UsageInfo.fromMap(Map map) => UsageInfo(
-      firstTimeStamp: map['firstTimeStamp'],
-      lastTimeStamp: map['lastTimeStamp'],
-      lastTimeUsed: map['lastTimeUsed'],
-      totalTimeInForeground: map['totalTimeInForeground'],
-      packageName: map['packageName'],
-      lastTimeForegroundServiceUsed: map['lastTimeForegroundServiceUsed']);
+        firstTimeStamp: map['firstTimeStamp'],
+        lastTimeStamp: map['lastTimeStamp'],
+        lastTimeUsed: map['lastTimeUsed'],
+        totalTimeInForeground: map['totalTimeInForeground'],
+        packageName: map['packageName'],
+      );
 
   final String? packageName;
   final String? firstTimeStamp;
   final String? lastTimeStamp;
   final String? lastTimeUsed;
   final String? totalTimeInForeground;
-  final String? lastTimeForegroundServiceUsed;
 }
